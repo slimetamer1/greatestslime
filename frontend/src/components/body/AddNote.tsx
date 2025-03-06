dd57505d8d2ce9a474cb7826f5b1d3fc1d808cf3
pendonj14 <pendon.josephjr@gmail.com>
2025-03-06 23:41:48 +0800

    improved dark mode

--- frontend/src/components/body/AddNote.tsx
+++ frontend/src/components/body/AddNote.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import { Button } from "@/components/ui/button";
 import api from "../../api/api";
 import { Note } from "@/types/types";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
 
 interface AddNoteProps {
     setNotes: React.Dispatch<React.SetStateAction<Note[]>>;
@@ -10,7 +11,8 @@ interface AddNoteProps {
 
 const AddNote: React.FC<AddNoteProps> = ({ setNotes, showModalMessage }) => {
     const [content, setContent] = useState<string>("");
-
+    const {theme} = useTheme();
+    
     const getNotes = async () => {
         try {
             const res = await api.get("/api/notes/");
@@ -36,7 +38,7 @@ const AddNote: React.FC<AddNoteProps> = ({ setNotes, showModalMessage }) => {
     };
 
     return (
-        <div className="flex flex-col justify-between bg-[#B3B3B3] m-3 p-3 rounded-lg min-h-[200px] text-center motion-preset-expand motion-duration-1000">
+        <div className={`flex flex-col justify-between ${theme == 'dark' ? 'bg-[#2a2a2a]': 'bg-[#B3B3B3]'} m-3 p-3 rounded-lg min-h-[200px] text-center motion-preset-expand motion-duration-1000`}>
             <form onSubmit={createNote}>
                 <textarea
                     className="resize-none bg-inherit border-none outline-none text-center pt-[55px]"
--- frontend/src/components/body/Note.tsx
+++ frontend/src/components/body/Note.tsx
@@ -1,5 +1,5 @@
 import { MdOutlineEdit, MdDeleteOutline } from "react-icons/md";
-
+import { useTheme } from "next-themes";
 
 interface Note {
     id: number;
@@ -17,10 +17,10 @@ interface NotesProps {
 
 const Notes: React.FC<NotesProps> = ({ note, onDelete, onEdit }) => {
     const formattedDate = new Date(note.created_at).toLocaleDateString("en-US");
-
+    const { theme } = useTheme();
     return (
         <div
-            className={`flex flex-col justify-between ${note.bg_color || "bg-yellow-400"} 
+            className={`flex flex-col justify-between ${theme === 'dark' ? 'bg-[#323232]' : note.bg_color } 
                 m-3 p-3 rounded-lg min-h-[200px] text-center 
                 hover:scale-105 transition-transform duration-300 ease-out will-change-transform`}
         >
