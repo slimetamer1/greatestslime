d326a79fbb603790ad0b9f0126ec1280a84c968a
slimetamer1 <sarmiento.kyle322@gmail.com>
2025-03-07 01:10:59 +0800

    Delete backend directory

--- backend/api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Note
-
-# Register your models here.
-
-@admin.register(Note)
-class NoteAdmin(admin.ModelAdmin):
-    list_display = ('content', 'created_at', 'author')
--- backend/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("notes/", views.NoteListCreate.as_view(), name="note-list"),
-    path("notes/delete/<int:pk>/", views.NoteDelete.as_view(), name="delete-note"),
-    path("notes/<int:pk>/", views.NoteUpdate.as_view(), name='note-update'), 
-    path('users/<int:pk>/', views.UserDetailView.as_view(), name='user-detail'),
-]
